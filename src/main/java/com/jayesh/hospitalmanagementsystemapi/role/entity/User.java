0e7f60c9e34a6f9a62e57c11f4a53285192745b2
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-11 21:56:16 +0530

    Added User and Role Entity

---
+++ src/main/java/com/jayesh/hospitalmanagementsystemapi/role/entity/User.java
@@ -0,0 +1,37 @@
+package com.jayesh.hospitalmanagementsystemapi.role.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+            @JoinTable(
+                    name = "user_role",
+                    joinColumns = @JoinColumn(name = "user_id"),
+                    inverseJoinColumns = @JoinColumn(name = "role_id")
+            )
+    Set<Role> roles = new HashSet<>();
+
+    public User(String username, String password, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+}
