fc41b3ca028d3c9451902ef853ab13a3c4e062a9
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-11 22:10:52 +0530

    Added Response and Request DTOs

---
+++ src/main/java/com/jayesh/hospitalmanagementsystemapi/role/dto/AuthRequestDTO.java
@@ -0,0 +1,18 @@
+package com.jayesh.hospitalmanagementsystemapi.role.dto;
+import com.jayesh.hospitalmanagementsystemapi.role.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequestDTO {
+    private String username;
+    private String password;
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/jayesh/hospitalmanagementsystemapi/role/dto/AuthResponseDTO.java
@@ -0,0 +1,15 @@
+package com.jayesh.hospitalmanagementsystemapi.role.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String username;
+    private String message;
+}
